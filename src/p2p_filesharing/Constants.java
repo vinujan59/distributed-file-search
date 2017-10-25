49ec74bf4e2e5ab09351105faa6e55b1d5570804
unknown <Jana93@bitbucket.org>
2017-10-24 18:23:08 -0700

    implemented register and register ok

---
+++ src/p2p_filesharing/Constants.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p2p_filesharing;
+
+/**
+ *
+ * @author USER
+ */
+public class Constants {
+    
+    public static String BS_IP = "127.0.0.1";
+    public static int BS_PORT = 55555;
+    
+}
